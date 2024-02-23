00957dc1b1d001dba12c8c900edcd3c954528e2a
Ayush <me21b1076@iiitdm.ac.in>
2024-02-24 00:14:33 +0530

    Advanced Setip - Util files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
 dotenv.config()
+
 connectDB()
+    .then(() => {
+        const PORT = process.env.PORT || 5000; 
+        app.listen(PORT, () => console.log(`Listning to PORT ${PORT}`))
+    })
+    .catch((err)=>console.error("Databse connection failed ",err))
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+const asyncHandler = (requstHandler) =>{
+    (req, res, next) => {
+        Promise.resolve(requstHandler(req, res, next))
+            .catch((err)=> next(err))
+    }
+}
+
+export {asyncHandler}
