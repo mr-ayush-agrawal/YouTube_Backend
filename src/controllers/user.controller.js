8b9e96b3ef76888738b2573fb0c9b6b6baa0bd85
Ayush <me21b1076@iiitdm.ac.in>
2024-02-25 14:09:51 +0530

    Login and Logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,26 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiError } from '../utils/ApiError.js'
 import { User } from '../models/users.models.js'
-import {uploadOnCloudinary} from '../utils/cloudinary.js'
+import { uploadOnCloudinary } from '../utils/cloudinary.js'
 import { ApiResponse } from "../utils/ApiResponse.js"
+
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+        // This is use to save the data to the data base
+
+        return { accessToken, refreshToken };
+    }
+    catch (err) {
+        // console.error("Error While generating token : ", err)
+        throw new ApiError(500, "Something went wrong while generating tokens");
+    }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
     /* Steps for creation
@@ -27,20 +45,20 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // 2
     const existedUser = await User.findOne({
-        $or : [{email}, {username}]
+        $or: [{ email }, { username }]
     })
 
     // 3
-    if(existedUser)
+    if (existedUser)
         throw new ApiError(409, 'User with email or username alredy exist')
 
     // 4
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    if(!avatarLocalPath)
+    if (!avatarLocalPath)
         throw new ApiError(400, "Avatar is required")
 
     let coverImageLocalPath
-    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0)
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0)
         coverImageLocalPath = req.files.coverImage[0].path
 
 
@@ -66,7 +84,7 @@ const registerUser = asyncHandler(async (req, res) => {
         "-password -refreshToken"
     )
 
-    if(!createdUser)
+    if (!createdUser)
         throw new ApiError(500, "Something went wrong while registering the User");
 
     // 9
@@ -76,4 +94,75 @@ const registerUser = asyncHandler(async (req, res) => {
 
 })
 
-export { registerUser }
+
+const loginUser = asyncHandler(async (req, res) => {
+    /* Steps 
+    // - Refresh token check
+    - get data from form 
+    - search user and check passwod
+    - create access and reresh token
+    - send cokie
+    */
+
+    const { username, email, password } = req.body
+
+    if (!username && !email) {
+        throw new ApiError(400, "Anyone of the 2 Email or username is required")
+    }
+
+    let reqUser = await User.findOne({
+        $or: [{ email }, { username }]
+    })
+
+
+    if (!reqUser) {
+        throw new ApiError(404, "No User Found");
+    }
+
+    const isValidPass = await reqUser.isCorrectPassword(password)
+    if (!isValidPass) {
+        throw new ApiError(403, "Wrong Password")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(reqUser._id)
+
+    const loggedInUser = await User.findById(reqUser._id).select("-password -refreshToken")
+
+    // The above call can be omited -> Tried but failed to remove password from regUser
+    // 2 Possible solution -> only taking the required feilds abs pass aas another obj
+    // -> Replacing the content in teh password as NULL/Hidden;
+
+    const cookieOptions = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, cookieOptions)
+        .cookie("refreshToken", refreshToken, cookieOptions)
+        .json(new ApiResponse(200, { user: loggedInUser, accessToken, refreshToken }, "Login SuccessFul "))
+})
+
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id, {
+        $set: {
+            refreshToken: undefined
+        }
+    }, {
+        new: true
+    }
+    )
+    const cookieOptions = {
+        httpOnly: true,
+        secure: true
+    }
+    return res
+        .status(200)
+        .clearCookie('accessToken', cookieOptions)
+        .clearCookie('refreshToken', cookieOptions)
+})
+
+export { registerUser, loginUser, logoutUser }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
 import { upload } from '../middlewares/multer.middleware.js'
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { verifyJWT } from '../middlewares/auth.middleware.js'
 
 const router = Router();
 
@@ -15,5 +16,10 @@ router.route('/register').post(
             maxCount: 1
         }
     ]), registerUser)
+
+router.route('/login').post(loginUser)
+
+//secured Routes
+router.route('/logout').post(verifyJWT, logoutUser)
 
 export default router;
